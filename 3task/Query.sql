ce050dae56c6a2f85976269d0bc98cd2fc5345c4
RomanDubinin <Roman230394@yandex.ru>
2014-12-08 02:45:30 +0500

    подсчёт богатства пока без функций

--- 3task/Query.sql
+++ 3task/Query.sql
@@ -7,12 +7,13 @@ drop table tmp1;
 if object_id('ExchengesForMyPurse', 'U') IS NOT NULL
 drop table ExchengesForMyPurse;
 
+if object_id('SeparatedWealth', 'U') IS NOT NULL
+drop table SeparatedWealth;
+
 select ExchangeRates.idCurrencyFrom, ExchangeRates.idCurrencyTo, ExchangeRates.price, Purse.amount
 into tmp1
 from ExchangeRates, Purse
 where ExchangeRates.idCurrencyFrom = Purse.idCurrency
-
-
 
 select idCurrencyFrom, idCurrencyTo, price, amount
 into ExchengesForMyPurse
@@ -21,3 +22,13 @@ where idCurrencyTo = 2
 
 select *
 from ExchengesForMyPurse
+
+select idCurrencyFrom, price * amount as wealth
+into SeparatedWealth
+from ExchengesForMyPurse
+
+select *
+from SeparatedWealth
+
+select sum(wealth)
+from SeparatedWealth
