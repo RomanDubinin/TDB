331ec1694a46a604021223e4ae889d1ceea60ca4
RomanDubinin <Roman230394@yandex.ru>
2014-12-08 22:44:04 +0500

    процедура

--- 3task/Query.sql
+++ 3task/Query.sql
@@ -1,34 +1,37 @@
-use [roman.dubinin]
+create procedure TotalWealth @idCurrency int as
+begin 
+	if object_id('tmp1', 'U') IS NOT NULL
+	drop table tmp1;
+	
+	if object_id('ExchengesForMyPurse', 'U') IS NOT NULL
+	drop table ExchengesForMyPurse;
+	
+	if object_id('SeparatedWealth', 'U') IS NOT NULL
+	drop table SeparatedWealth;
+	
+	select ExchangeRates.idCurrencyFrom, ExchangeRates.idCurrencyTo, ExchangeRates.price, Purse.amount
+	into tmp1
+	from ExchangeRates, Purse
+	where ExchangeRates.idCurrencyFrom = Purse.idCurrency
+	
+	select idCurrencyFrom, idCurrencyTo, price, amount
+	into ExchengesForMyPurse
+	from tmp1
+	where idCurrencyTo = @idCurrency
+	
+	select *
+	from ExchengesForMyPurse
+	
+	select idCurrencyFrom, price * amount as wealth
+	into SeparatedWealth
+	from ExchengesForMyPurse
+	
+	select *
+	from SeparatedWealth
+	
+	select sum(wealth) as worth
+	from SeparatedWealth
+end
 
+exec TotalWealth @idCurrency = 3
 
-if object_id('tmp1', 'U') IS NOT NULL
-drop table tmp1;
-
-if object_id('ExchengesForMyPurse', 'U') IS NOT NULL
-drop table ExchengesForMyPurse;
-
-if object_id('SeparatedWealth', 'U') IS NOT NULL
-drop table SeparatedWealth;
-
-select ExchangeRates.idCurrencyFrom, ExchangeRates.idCurrencyTo, ExchangeRates.price, Purse.amount
-into tmp1
-from ExchangeRates, Purse
-where ExchangeRates.idCurrencyFrom = Purse.idCurrency
-
-select idCurrencyFrom, idCurrencyTo, price, amount
-into ExchengesForMyPurse
-from tmp1
-where idCurrencyTo = 2
-
-select *
-from ExchengesForMyPurse
-
-select idCurrencyFrom, price * amount as wealth
-into SeparatedWealth
-from ExchengesForMyPurse
-
-select *
-from SeparatedWealth
-
-select sum(wealth)
-from SeparatedWealth
