f2668af1ea388bd6d41d1e679288c509135a97b3
RomanDubinin <Roman230394@yandex.ru>
2014-12-14 01:53:06 +0500

    нашёл все Х точек пересечения

---
+++ 4task/AllIntersections.sql
@@ -0,0 +1,6 @@
+﻿select 
+	dbo.IntersectionOfLines(0, tariff1.licenseFee, tariff2.feeOver, -(tariff2.limit - tariff2.licenseFee/tariff2.feeOver)*tariff2.feeOver),
+	dbo.IntersectionOfLines(0, tariff2.licenseFee, tariff1.feeOver, -(tariff1.limit - tariff1.licenseFee/tariff1.feeOver)*tariff1.feeOver),
+	dbo.IntersectionOfLines(tariff1.feeOver, -tariff1.limit + tariff1.licenseFee, tariff2.feeOver, -tariff2.limit + tariff2.licenseFee)
+from Tariffs as tariff1
+cross join Tariffs as tariff2
