3c06057922b7f554ffd5ed5db6e1dc42f7423584
RomanDubinin <Roman230394@yandex.ru>
2014-12-13 16:35:46 +0500

    первый запросик

---
+++ 5task/Departures.sql
@@ -0,0 +1,20 @@
+﻿create procedure Departures 
+	@boatType varchar(50), 
+	@dateFrom date,
+	@dateTo date 
+as
+begin 
+	select Fishery.departure, BoatPassport.boatName, FishCatch.codfish + FishCatch.herring + FishCatch.crab as 'Total Catch'
+	from Fishery
+	join BoatPassport
+	on Fishery.idBoat = BoatPassport.boatId
+	join FishCatch
+	on Fishery.idFishCatch = FishCatch.fishCatchId
+	where 
+		Fishery.departure >= @dateFrom and 
+		Fishery.departure <= @dateTo and
+		BoatPassport.boatType = @boatType
+	order by Fishery.departure
+end
+
+drop procedure Departures;
---
+++ 5task/Query.sql
@@ -0,0 +1,4 @@
+﻿use [sql]
+
+exec Departures @boatType = 'Type1', @dateFrom = '2014-11-01', @dateTo = '2014-11-21'
+
