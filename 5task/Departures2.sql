b555183d56bfb09f72036e7550f914ae0910378b
RomanDubinin <Roman230394@yandex.ru>
2014-12-13 19:09:53 +0500

    последний

---
+++ 5task/Departures2.sql
@@ -0,0 +1,18 @@
+﻿create procedure Departures2
+	@dateFrom date,
+	@dateTo date 
+as
+begin 
+	select Fishery.departure, Fishery.arrival, BoatPassport.boatName, FishCatch.codfish + FishCatch.herring + FishCatch.crab as 'Total Catch'
+	from Fishery
+	join BoatPassport
+	on Fishery.idBoat = BoatPassport.boatId
+	join FishCatch
+	on Fishery.idFishCatch = FishCatch.fishCatchId
+	where 
+		Fishery.departure >= @dateFrom and 
+		Fishery.arrival <= @dateTo
+	order by Fishery.departure
+end
+
+drop procedure Departures2;
--- 5task/Query.sql
+++ 5task/Query.sql
@@ -6,3 +6,6 @@ exec AddFishingPlace @placeName = 'pl4', @x = 1, @y = 1
 
 exec BoatsOnPlace @placeName = 'Place1', @qality = 'good'
 
+exec Departures2 @dateFrom = '2014-11-08', @dateTo = '2014-11-25'
+
+
